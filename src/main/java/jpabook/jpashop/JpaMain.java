13533a2cbbab1d15222f6ce741781743bc18d226
HOSOO KANG <hosoo3513@gmail.com>
2022-05-20 13:29:33 +0900

    .

--- src/main/java/jpabook/jpashop/JpaMain.java
+++ src/main/java/jpabook/jpashop/JpaMain.java
@@ -8,7 +8,6 @@ import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
 
-
 public class JpaMain {
 
     public static void main(String[] args) {
