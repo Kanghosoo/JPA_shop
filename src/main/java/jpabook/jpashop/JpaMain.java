7ef04c6d171e9fd85e629839f4c81d63ebfdd244
HOSOO KANG <hosoo3513@gmail.com>
2022-05-26 13:07:25 +0900

    ...

--- src/main/java/jpabook/jpashop/JpaMain.java
+++ src/main/java/jpabook/jpashop/JpaMain.java
@@ -1,12 +1,12 @@
 package jpabook.jpashop;
 
-import jpabook.jpashop.domain.Order;
-import jpabook.jpashop.domain.OrderItem;
+        import jpabook.jpashop.domain.Order;
+        import jpabook.jpashop.domain.OrderItem;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.EntityTransaction;
-import javax.persistence.Persistence;
+        import javax.persistence.EntityManager;
+        import javax.persistence.EntityManagerFactory;
+        import javax.persistence.EntityTransaction;
+        import javax.persistence.Persistence;
 
 public class JpaMain {
 
